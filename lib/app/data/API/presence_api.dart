9d2c05e2275dbbc8dcac80c49314dfe6fd4d77fd
2212010012 <muhammadqaddasa@gmail.com>
2024-06-11 20:50:42 +0700

    presence_api_update

--- lib/app/data/API/presence_api.dart
+++ lib/app/data/API/presence_api.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_application_absensi/app/data/api_client.dart';
+import 'package:intl/intl.dart';
 
 class PresenceApi extends ApiClient {
   Future<Response> absenMasuk({
@@ -9,6 +10,9 @@ class PresenceApi extends ApiClient {
     required String waktuAbsenMasuk,
   }) async {
     try {
+      final DateTime now = DateTime.now();
+      final DateFormat formatter = DateFormat('yyyy-MM-dd');
+      final String formatted = formatter.format(now);
       var response = await dio.post(
         '$baseUrl/pegawai/absen',
         data: {
@@ -21,6 +25,47 @@ class PresenceApi extends ApiClient {
             Options(contentType: Headers.formUrlEncodedContentType, headers: {
           'Authorization': 'Bearer $accesstoken',
         }),
+      );
+      return response;
+    } on DioException catch (e) {
+      return e.response!;
+    }
+  }
+
+  Future<Response> cekAbsenHariIni({
+    required String accesstoken,
+    required String usersId,
+    required String waktuHariIni,
+  }) async {
+    try {
+      var response = await dio.get(
+        '$baseUrl/cek_absen_hari_ini/$usersId/$waktuHariIni',
+        options: Options(
+          contentType: Headers.formUrlEncodedContentType,
+          headers: {
+            'Authorization': 'Bearer $accesstoken',
+          },
+        ),
+      );
+      return response;
+    } on DioException catch (e) {
+      return e.response!;
+    }
+  }
+
+  Future<Response> absenHistory({
+    required String accesstoken,
+    required String usersId,
+  }) async {
+    try {
+      var response = await dio.get(
+        '$baseUrl/absen_history/$usersId',
+        options: Options(
+          contentType: Headers.formUrlEncodedContentType,
+          headers: {
+            'Authorization': 'Bearer $accesstoken',
+          },
+        ),
       );
       return response;
     } on DioException catch (e) {
